e7d5fcba85689cf975f3db82a1e9cc6f2868321b
Masanori Iguchi <masa@iguchi.biz>
2015-05-07 11:57:46 +0900

    Modify all the quize3

---
+++ easy_questions/quiz3/ex03.rb
@@ -0,0 +1,10 @@
+#I n the previous exercise we added Dino to our array like this:
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones << "Dino"
+
+# We could have used either Array#concat or Array#push to add Dino to the family.
+# How can we add multiple items to our array? (Dino and Hoppy)
+
+flintstones.push("Dino").push("Hoppy")
+# or
+flintstones.concat(%w(Dino Hoppy))
